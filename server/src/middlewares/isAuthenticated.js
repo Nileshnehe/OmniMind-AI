cbde859b3a8fab34e129997ee4185a205f7dbe5e
Nileshnhe <nehenilesh111@gmail.com>
2026-06-08 17:43:20 +0530

    feat(auth): implement isAuthenticated middleware for token validation

---
+++ server/src/middlewares/isAuthenticated.js
@@ -0,0 +1,28 @@
+import jwt from "jsonwebtoken";
+
+const isAuthenticated = (req, res, next) => {
+    try {
+
+        const authHeader = req.headers.authorization;
+
+        if (!authHeader || !authHeader.startsWith('Bearer ')) {
+            return res.status(401).json({
+                success: false,
+                message: 'Access token missing',
+            })
+        }
+
+        const token = authHeader.split(' ')[1];
+
+        const decoded = jwt.verify(token, process.env.JWT_ACCESS_TOKEN)
+
+        req.user = decoded
+
+        next()
+    } catch (error) {
+        return res.status(401).json({
+            success: false,
+            message: 'Invalid or expired token',
+        })
+    }
+}
