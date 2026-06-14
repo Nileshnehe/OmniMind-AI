951b8b81ab8a63091cbf8158d1b97292def02ddd
Nileshnhe <nehenilesh111@gmail.com>
2026-06-14 19:30:45 +0530

    feat(app): replace StrictMode and App component with Redux Provider and RouterProvider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './app/App'
+import { Provider } from 'react-redux'
+import {store} from './store/app.store'
+import { routes } from './app/app.route'
+import { RouterProvider } from 'react-router'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <Provider store={store}>
+    <RouterProvider router={routes} />
+  </Provider>
 )
