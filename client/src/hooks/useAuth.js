113f135167089ef351ea20c878e8581c3903fc1f
Nileshnhe <nehenilesh111@gmail.com>
2026-06-15 09:01:58 +0530

    perf(auth): memoize auth action dispatches using useCallback inside useAuth hook

--- client/src/hooks/useAuth.js
+++ client/src/hooks/useAuth.js
@@ -1,5 +1,6 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { loginUserThunk,registerUserThunk,clearAuthErrors,resetRegisterStatus } from '../store/slices/auth.slice';
+import { useCallback } from 'react';
 
 export const useAuth = () => {
   const dispatch = useDispatch();
@@ -7,21 +8,39 @@ export const useAuth = () => {
   // Extract tracking points out from state store registry
   const { user, isAuthenticated, loading, error, registerSuccess } = useSelector((state) => state.auth);
 
-  const login = (email, password) => {
+  // const login = (email, password) => {
+  //   dispatch(loginUserThunk({ email, password }));
+  // };
+
+  // const register = (username, email, password) => {
+  //   dispatch(registerUserThunk({ username, email, password }));
+  // };
+
+  // const clearErrors = () => {
+  //   dispatch(clearAuthErrors());
+  // };
+
+  // const resetRegisterFlag = () => {
+  //   dispatch(resetRegisterStatus());
+  // }; 
+
+  
+
+  const login = useCallback((email, password) => {
     dispatch(loginUserThunk({ email, password }));
-  };
+  }, [dispatch]);
 
-  const register = (username, email, password) => {
+  const register = useCallback((username, email, password) => {
     dispatch(registerUserThunk({ username, email, password }));
-  };
+  }, [dispatch]);
 
-  const clearErrors = () => {
+  const clearErrors = useCallback(() => {
     dispatch(clearAuthErrors());
-  };
+  }, [dispatch]);
 
-  const resetRegisterFlag = () => {
+  const resetRegisterFlag = useCallback(() => {
     dispatch(resetRegisterStatus());
-  };
+  }, [dispatch]);
 
   return {
     user,
