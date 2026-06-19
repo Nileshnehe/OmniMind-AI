e0c627a691a1b6e3eb1e7039eee70a9ffa90063b
Nileshnhe <nehenilesh111@gmail.com>
2026-06-19 19:14:15 +0530

    feat(chat): setup endpoints for fetching and deleting chats
    Added protected routes for getChats, getMessage, and deleteChat using the authUser middleware.

--- server/src/routes/chat.route.js
+++ server/src/routes/chat.route.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { sendMessage } from "../controllers/chat.controller.js";
+import { sendMessage, getChats, getMessage, deleteChat } from "../controllers/chat.controller.js";
 import { authUser } from "../middleware/auth.middleware.js";
 
 const chatRouter = Router();
 
-chatRouter.post("/message", authUser, sendMessage)
-
+chatRouter.post("/message", authUser, sendMessage);
+chatRouter.get("/", authUser, getChats);
+chatRouter.get("/:chatId/messages", authUser, getMessage);
+chatRouter.delete("/delete/:chatId", authUser, deleteChat);
 export default chatRouter;
