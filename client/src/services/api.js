c5725482e6961954762b19a94888658d57813c97
Nileshnhe <nehenilesh111@gmail.com>
2026-06-23 16:01:31 +0530

    chore: add gitignore file for client directory

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: import.meta.env.BACKEND_URL || 'http://localhost:3000/api',
   timeout: 10000,
   withCredentials: true,
   headers: {
