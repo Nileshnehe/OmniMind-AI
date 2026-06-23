5140e57ad819ff4bc6a4a84be5e2fc7fb25877d3
Nileshnhe <nehenilesh111@gmail.com>
2026-06-23 16:15:01 +0530

    feat: initialize express application with cors, logging, and routing middleware

--- server/src/app.js
+++ server/src/app.js
@@ -8,7 +8,7 @@ import chatRouter from "./routes/chat.route.js";
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://omni-ai-theta-eight.vercel.app || http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
 }))
