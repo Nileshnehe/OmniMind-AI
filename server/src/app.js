97230550786a7779fb7bebbd6686fda0760fe84e
Nileshnhe <nehenilesh111@gmail.com>
2026-06-23 19:13:42 +0530

    feat: initialize express application with middleware and route configuration

--- server/src/app.js
+++ server/src/app.js
@@ -9,7 +9,7 @@ import chatRouter from "./routes/chat.route.js";
 const app = express();
 
 app.use(cors({
-    origin: configData.FRONTEND_URL || "http://localhost:5173",
+    origin: configData.FRONTEND_URL ? [configData.FRONTEND_URL, "http://localhost:5173"] : "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
 }))
