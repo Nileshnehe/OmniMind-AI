5e5a4cf36beda309011b4f2d8222bc5a70f57504
Nileshnhe <nehenilesh111@gmail.com>
2026-06-23 16:29:19 +0530

    feat: initialize socket.io server with CORS configuration and connection handling

--- server/src/sockets/server.socket.js
+++ server/src/sockets/server.socket.js
@@ -5,7 +5,7 @@ let io;
 export function initSocket(httpServer) {
     io = new Server(httpServer, {
         cors: {
-            origin: "http://localhost:5173",
+            origin: "https://omni-ai-theta-eight.vercel.app",
             credentials: true,
         }
     });
